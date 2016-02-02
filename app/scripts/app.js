e70724461b6f0ad8de86d8b916eee14cbc755762
Benni Russell <bennirussell@gmail.com>
2016-02-02 22:47:21 +0000

    added new to appJS

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,30 +8,25 @@
  *
  * Main module of the application.
  */
-angular
-  .module('ticApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+// app.js
+var ticApp = angular.module('ticApp', ['ui.router']);
+
+ticApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'views/main.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            url: '/about',
+            templateUrl: 'views/about.html'
+        });
+        
+});
