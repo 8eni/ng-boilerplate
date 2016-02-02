654ed3a8b531f7e77751d7e2342683a521223340
Benni Russell <bennirussell@gmail.com>
2016-02-02 21:50:08 +0000

    initialize tracked changes

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ticApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ticApp
+ */
+angular.module('ticApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
